42135cdd0a0a2781b929d470b69c5fc73a86759e
Nour Zouari <153989236+Nour-Zouari@users.noreply.github.com>
2026-01-12 23:18:54 +0100

    Clean up run method by removing sample data
    Removed sample data creation and output statements from the run method.

--- src/main/java/com/example/MembreServiceApplication.java
+++ src/main/java/com/example/MembreServiceApplication.java
@@ -37,44 +37,6 @@ public class MembreServiceApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Etudiant etd1=Etudiant.builder()
-				 .cin("123456")
-				 .password("pass1")
-				 .cv("cv1")
-				 .nom("abid")
-				 .prenom("youssef)")
-				 .build();
-		etudiantRepository.save(etd1);
-		EnseignantChercheur ens1=EnseignantChercheur.builder().cin("48579").nom("nour").build();
-		enseignantChercheurRepository.save(ens1);
-		 Etudiant etd2 = Etudiant.builder()
-		            .nom("alami")
-		            .prenom("karim")
-		            .build();
-		    etudiantRepository.save(etd2);
-		    List<Etudiant> etudiantsTrouves = etudiantRepository.chercher("a%");
-
-		   
-		    System.out.println("--- Étudiants trouvés ---");
-		    etudiantsTrouves.forEach(etd -> System.out.println(etd.getNom() + " " + etd.getPrenom()));
-		    System.out.println("-------------------------");
-		    
-		    Membre m= memberService.findMember(1L);
-			 m.setCv("cv1.pdf");
-			 memberService.updateMember(m);
-			 
-			// Delete a Member
-			 
-			 memberService.affecter_Etudiant_Enseignant(1L, 2L);
-			 memberService.affecter_Etudiant_Enseignant(3L, 2L);
-			 List<Etudiant> etudiantsAffectés=memberService.findEtudiantByEnseignant(ens1);
-			 System.out.println("--- Étudiants Affectés A l'enseignant ---");
-			 etudiantsAffectés.forEach(etd -> System.out.println(etd.getNom() + " " + etd.getPrenom()));
-			    System.out.println("-------------------------");
-			    
-			    System.out.println("publication 1"+publicationProxy.findPublicationById(1L));
-			    memberService.affecterauteurTopublication(2L, 1L);
-			    memberService.affecterauteurTopublication(2L, 2L);
 			 
 	}
 	
